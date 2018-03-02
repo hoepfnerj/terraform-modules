bccf61674d3f3750a0a5d85e764d59c1e0045acc
Jonas Höpfner <hoepfnej@swisslife-ad.de>
2018-03-02 16:34:19 +0100

    added sample modules

---
+++ subnet/vars.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  description = "The id of the vpc"
+}
+
+variable "cidr" {
+  description = "The cidr of the subnet"
+}
+
+variable "availability_zone" {
+  description = "The availability zone of the subnet"
+}
---
+++ vpc/vars.tf
@@ -0,0 +1,3 @@
+variable "vpc_cidr_block" {
+  description = "The cidr block of the vpc"
+}
