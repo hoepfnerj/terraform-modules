6cc40f97d3be77e0e99f9ae6f964e15e8363f33e
Jonas Höpfner <hoepfnej@swisslife-ad.de>
2018-03-02 16:57:12 +0100

    added tag to vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,4 +2,8 @@ resource "aws_vpc" "test-vpc" {
   cidr_block           = "${var.vpc_cidr_block}"
   enable_dns_hostnames = true
   enable_dns_support   = true
+  
+  tags {
+    Name = "HelloWorld"
+  }
 }
